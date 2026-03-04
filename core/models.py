6cae26dcc65de7f645398e6b2f5cb89f71ac25c5
Karl-Mitx <kescobarc14@miumg.edu.gt>
2026-03-04 15:24:44 -0600

    Se añadio la tabla catedratico

--- core/models.py
+++ core/models.py
@@ -10,3 +10,14 @@ class Curso(models.Model):
 
     class Meta:
         db_table = 'curso'
+
+class Catedratico(models.Model):
+    primer_nombre = models.CharField(max_length=100)
+    segundo_nombre = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return f"{self.primer_nombre} {self.segundo_nombre}"
+
+    class Meta:
+        db_table = 'catedratico'
